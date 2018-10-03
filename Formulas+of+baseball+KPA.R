dcf95503daf081cf3db2d0b2718c44f310d1aebf
KJKILLPACK <43452380+KJKILLPACK@users.noreply.github.com>
2018-10-03 09:29:23 -0600

    Drafting of Playoff Analysis

---
+++ Formulas+of+baseball+KPA.R
@@ -0,0 +1,16 @@
+#'Sabermetric (W%) Formula
+#'
+#'
+#'
+#'Calculates the win percentage likely hood of a team based on (Rs)^2/((Rs)^2 + (RsA)^2)
+#' @param Rs Vector of numbers for Run's Scored in a season
+#' @param RsA Vector of numbers for Run's Scored in a season
+#' @return The predictied win Percentage for a game written as integer
+sabermtrkk <- function(x, y){
+  z <- (sum(x, na.rm = TRUE))
+  u <- (sum(y, na.rm = TRUE))
+  Rs2 <- (z)^2
+  RA2 <- (u)^2
+  RR  <- (Rs2/(Rs2+RA2))
+  return(RR)
+}
